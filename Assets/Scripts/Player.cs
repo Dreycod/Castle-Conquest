2b94a16836b11d3ee065c696b5a694d797f063e5
Dreycod <84332780+Dreycod@users.noreply.github.com>
2022-07-10 13:40:57 +0200

    Background layers + Double jump fix + Stuck in a wall while walking fix + Multiple jumping in wall fix + added gravity/jumps

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,27 +6,49 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float JumpSpeed = 15f;
    // public float Velocity = 300.0f;
     Rigidbody2D myRigidBody2D;
     Animator myAnimator;
+    BoxCollider2D myBoxCollider2D;
+    PolygonCollider2D myPolygonCollider2D;
 
-    // Start is called before the first frame update
+// Start is called before the first frame update
     void Start()
     {
         myRigidBody2D = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
+        myPolygonCollider2D = GetComponent<PolygonCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        int GroundLayer = LayerMask.GetMask("Ground");
+        bool IsTouchingGround = myPolygonCollider2D.IsTouchingLayers(GroundLayer);
+
+        if (!IsTouchingGround){return;};
+
+        bool IsJumping = CrossPlatformInputManager.GetButtonDown("Jump");
+
+        if (IsJumping)
+        {
+            Vector2 jumpVelocity = new Vector2(myRigidBody2D.velocity.x, JumpSpeed);
+            myRigidBody2D.velocity = jumpVelocity;
+        }
     }
     private void Run()
     {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
 
-        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed,0f);
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody2D.velocity.y);
         myRigidBody2D.velocity = playerVelocity;
         FlipSprite();
         ChangingToRunningState();
